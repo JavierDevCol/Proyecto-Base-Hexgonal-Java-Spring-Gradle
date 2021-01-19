e850ca1260b7c48e6e850f7d12f56fac722cd1e3
randon93 <jbgm93@gmail.com>
2021-01-19 11:24:41 -0500

    inicio2

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,6 +2,6 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HOLA PERRITOS, MI PRIMER PROYECTO DESDE CERO(O) CON GRADLE");
+        System.out.println("HOLA  PERRITOS, MI PRIMER PROYECTO DESDE CERO(O) CON GRADLE");
     }
 }
