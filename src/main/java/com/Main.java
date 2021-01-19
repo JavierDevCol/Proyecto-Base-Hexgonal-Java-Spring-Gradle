a5280fe1589fadb522a0f44df7c0e21a0941eff0
randon93 <jbgm93@gmail.com>
2021-01-19 12:37:08 -0500

    AGREGANDO SUBMODULOS RESTANTES Y CONFIGURACION A LA CONEXION BD

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,5 +1,10 @@
 package com;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan({"com"})
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("HOLA  PERRITOS, MI PRIMER PROYECTO DESDE CERO(O) CON GRADLE");
