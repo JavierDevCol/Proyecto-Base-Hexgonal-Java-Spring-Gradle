f9118f0cb9c61ad3e0929d9c33a3592fb0ccb44c
onad-dev <jbgm93@gmai.com>
2021-01-28 14:07:27 -0500

    agregando dependencia en Infraestructura para que funcionara ome

--- infraestructura/src/main/java/com/persona/PersonaEntity.java
+++ infraestructura/src/main/java/com/persona/PersonaEntity.java
@@ -1,4 +1,24 @@
 package com.persona;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "persona")
 public class PersonaEntity {
+
+    @Id
+    private Long id;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private String clave;
+
+    @Column(name = "fecha_creacion")
+    private LocalDateTime fechaCreacion;
 }
